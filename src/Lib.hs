54c86979b8aa35e8819bddc3b4d4ec85f08d7760
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-08-23 13:51:26 -0400

    milestone translation

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,10 @@ module Lib
 
 import qualified Data.Vector as Vector
 
+import Control.Monad.IO.Class
+  ( liftIO
+  )
+
 import Text.Printf
   ( printf
   )
@@ -39,11 +43,16 @@ import GitHub.Data.Name
   )
 
 import GitHub.Data.Milestone
-  ( Milestone(milestoneNumber)
+  ( Milestone(milestoneNumber, milestoneTitle)
   )
 
 import GitHub.Auth
   ( Auth(OAuth)
+  )
+
+import GitHub.Data.Id
+  ( Id
+  , mkId
   )
 
 import GitHub.Endpoints.Issues
@@ -51,11 +60,15 @@ import GitHub.Endpoints.Issues
   , NewIssue(..)
   , issue
   , createIssue
-  , mkId
+  )
+
+import GitHub.Endpoints.Issues.Milestones
+  ( milestones'
   )
 
 someFunc
-  :: Text    -- Personal authentication token
+  :: Text    -- Personal authentication token.  This definitely must have at
+             -- least the "repo" privilege!
   -> Text    -- Source owner name
   -> Text    -- Source repository name
   -> Integer -- Source issue number
@@ -82,14 +95,55 @@ someFunc token sourceOwner sourceRepo sourceIssue destOwner destRepo = do
         Left  err   -> putStrLn $ printf "Create failed: %s" $ show err
         Right moved -> putStrLn "ok"
 
+
 move
   :: Auth                    -- Credentials for authentication
   -> Name Owner              -- The destination owner
   -> Name Repo               -- The destination repository
   -> Issue                   -- The issue to "move
   -> IO (Either Error Issue) -- The issue might have been moved
-move auth owner repo issue =
-  createIssue auth owner repo $ issueToNewIssue issue
+move auth owner repo issue = do
+  maybeMilestoneId <- maybeTranslatedMilestone auth owner repo issue
+  let newIssue = (issueToNewIssue issue)
+                 { newIssueMilestone = maybeMilestoneId
+                 }
+  putStrLn $ show newIssue
+  createIssue auth owner repo newIssue
+
+
+maybeTranslatedMilestone
+  :: Auth                    -- Credentials for authentication
+  -> Name Owner              -- The destination owner
+  -> Name Repo               -- The destination repository
+  -> Issue
+  -> IO (Maybe (Id Milestone))
+maybeTranslatedMilestone auth owner repo issue =
+  case issueMilestone issue of
+    Nothing              ->
+      return Nothing
+    Just sourceMilestone -> do
+      maybeMilestoneId <- translateMilestone auth owner repo sourceMilestone
+      case maybeMilestoneId of
+        Nothing ->
+          return Nothing
+        Just id ->
+          return $ Just id
+
+translateMilestone
+  :: Auth                      -- Credentials for authentication
+  -> Name Owner                -- The owner of the repository in which to seek a milestone
+  -> Name Repo                 -- The repository in which to seek a milestone
+  -> Milestone                 -- The milestone to seek a translation of
+  -> IO (Maybe (Id Milestone)) -- The id of the a milestone matching the above.
+translateMilestone auth owner repo sourceMilestone = do
+  -- Look up the milestones and find one with a matching title
+  result <- liftIO $ milestones' (Just auth) owner repo
+  case result of
+    Right milestones ->
+      case Vector.find (\m -> milestoneTitle m == milestoneTitle sourceMilestone) milestones of
+        Just milestone -> return $ Just $ milestoneNumber milestone
+        otherwise -> return Nothing
+    otherwise -> return Nothing
 
 
 issueToNewIssue
